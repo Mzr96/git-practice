6f16944b568c853ffefaf058a31e9c6314daa920
Maziar Moslemi <maziarmoslemi@gmail.com>
2020-03-17 11:11:05 +0330

    rm command is added

---
+++ test.py
@@ -0,0 +1,39 @@
+print('hello')
+'adding some bullshit for git'
+'''i'm going to practice checkout command in git'''
+'checkout command reverse your project one step'
+'git checkout -- rewards.py'
+'and it works before commit '
+
+'using reset command for unstaging'
+'git reset HEAD test.py'
+
+'amend command is used for editing last log or commit definition'
+'git commit --amend -m "new definition'
+
+'reset command is also used to get back to specific log'
+'git reset --hard dd9358f8ad323759571c1fc55926d0e0048900ce '
+
+'diff command compare curent status(before commiting) with recent one'
+'git diff'
+
+'git log -p give us log history with more details'
+'in other words it gives us exact changes'
+
+'git log --oneline gives us details in shorter terms '
+
+'by getting the hash code from previous step'
+'and using diff comand we can compare demanding log with curent status'
+'git diff 01cfa3d'
+
+'git blame test.py command shows log in diffrent but useful way'
+'i can not give more details it to hard to define it by words'
+'try it to undestand'
+
+'for changingin the existing file mv command is used'
+'''git mv 'current name' 'new dile name' '''
+
+'for deleting a specific file rm command is used'
+''' git rm 'filename' '''
+
+
